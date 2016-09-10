2af9930e1e72220b49af350596a316a29a4a7282
Jacob Banks <jacobanks@gmail.com>
2016-09-10 04:09:45 -0400

    Added frequency change from base64

--- FrequencyPlayer.js
+++ FrequencyPlayer.js
@@ -1,8 +1,20 @@
 context = new AudioContext();
 
+var base64String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+var testBase64String = document.getElementsByName('notefield').value;
+
+// create an array of the needed frequencies to play
+var frequencyArray = [];
+for (var i = 0; i < testBase64String.length; i++) {
+    var index = base64String.indexOf(testBase64String[i]);
+    var freq = (index * 250) + 4000;
+    frequencyArray.push(freq);
+}
+
 function playFrequency() {
     oscillator = context.createOscillator();
-    oscillator.frequency.value = 200;
+    oscillator.frequency.value = frequencyArray[0];
 
     oscillator.connect(context.destination);
 
