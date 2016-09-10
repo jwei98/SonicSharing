96cb0b4c52582c1b853dd797d5690dcc67e9270d
jwei98 <justin.lj.wei@gmail.com>
2016-09-10 04:23:09 -0400

    implemented user choosing notes

--- FrequencyPlayer.js
+++ FrequencyPlayer.js
@@ -2,17 +2,18 @@ context = new AudioContext();
 
 var base64String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-var testBase64String = document.getElementsByName('notefield').value;
-
-// create an array of the needed frequencies to play
-var frequencyArray = [];
-for (var i = 0; i < testBase64String.length; i++) {
-    var index = base64String.indexOf(testBase64String[i]);
-    var freq = (index * 250) + 4000;
-    frequencyArray.push(freq);
-}
-
 function playFrequency() {
+    console.log("gottem");
+    var testBase64String = document.getElementsByName('noteField')[0].value;
+
+    // create an array of the needed frequencies to play
+    var frequencyArray = [];
+    for (var i = 0; i < testBase64String.length; i++) {
+        var index = base64String.indexOf(testBase64String[i]);
+        var freq = (index * 250) + 4000;
+        frequencyArray.push(freq);
+    }
+
     oscillator = context.createOscillator();
     oscillator.frequency.value = frequencyArray[0];
 
